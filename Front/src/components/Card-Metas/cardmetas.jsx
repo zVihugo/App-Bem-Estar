86c1bc5407965920e24d4a42c8873e46833c2fd1
Matheus Kodi Y. <matheuskodi2@gmail.com>
2025-04-29 13:48:52 -0300

    feat: card metas na tela principal

---
+++ Front/src/components/Card-Metas/cardmetas.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './cardmetas.module.css';
+import { useState } from 'react';
+const CardMetas = () => {
+    const [metas, setMetas] = useState([
+        { id: 1, texto: 'Meditar por 10min', concluida: false },
+        { id: 2, texto: 'Dormir 8h por noites', concluida: false },
+      ]);
+    
+      const toggleMeta = (id) => {
+        setMetas(metas.map(meta =>
+          meta.id === id ? { ...meta, concluida: !meta.concluida } : meta
+        ));
+      };
+    
+      return (
+        <div className={styles.card_metas}>
+          <p> 🎯 Metas em andamento </p>
+          <ul className={styles.meta_list}>
+            {metas.map(meta => (
+              <li key={meta.id}>
+                <label className={styles.meta_item}>
+                  <input
+                    type="checkbox"
+                    checked={meta.concluida}
+                    onChange={() => toggleMeta(meta.id)}
+                    className={styles.checkbox}
+                  />
+                  <span className={meta.concluida ? 'concluida' : ''}>
+                    {meta.texto}
+                  </span>
+                </label>
+              </li>
+            ))}
+          </ul>
+            <button className={styles.button}>Ver todas as metas</button>
+        </div>
+      );
+};
+
+export default CardMetas;
--- Front/src/pages/Principal/Principal.jsx
+++ Front/src/pages/Principal/Principal.jsx
@@ -3,6 +3,7 @@ import CardWelcome from '../../components/Card-Welcome/cardwelcome'
 import CardAutoAvaliacao from '../../components/Card-AutoAvaliacao/cardautoavaliacao'
 import CardHumor from '../../components/Card-Humor/cardhumor'
 import CardAutoajuda from '../../components/Card-Autoajuda/cardautoajuda'
+import CardMetas from '../../components/Card-Metas/cardmetas'
 
 const Principal = () => {
   return (
@@ -11,6 +12,7 @@ const Principal = () => {
       <CardAutoAvaliacao />
       <CardHumor />
       <CardAutoajuda />
+      <CardMetas />
     </div>
   )
 }
