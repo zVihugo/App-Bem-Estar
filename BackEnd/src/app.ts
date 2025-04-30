21c2723559f2ce0a655958c2113689c986f38043
MauricioJrB <mjunior1205@gmail.com>
2025-04-30 19:28:43 -0300

    feat: instanciando middleware de conexao com o banco no app

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -1,8 +1,11 @@
 import cors from 'cors';
 import express from 'express';
+import { connectToDatabase } from './utils/prisma';
 
 const app = express();
 
 app.use(cors());
+
+connectToDatabase();
 
 export { app };
