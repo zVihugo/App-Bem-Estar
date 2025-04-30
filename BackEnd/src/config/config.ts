8f1964ce238f1292b75d8dbb6f748731429ae58f
MauricioJrB <mjunior1205@gmail.com>
2025-04-30 18:42:47 -0300

    feat: adicionado config para exportar variáveis de ambiente

---
+++ BackEnd/src/config/config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+  PORT: process.env.PORT,
+  SALT_ROUNDS: process.env.SALT_ROUNDS,
+  JWT_SECRET: process.env.JWT_SECRET as string,
+};
