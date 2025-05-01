0891412ed30334059865ffb8c66a674fd24e5abd
Luiza Xavier Nakahira <luizanakahira40@gmail.com>
2025-04-30 22:26:14 -0300

    Arrumando responsividade do Metas

--- Front/src/components/Card-Suasmetas/cardsuasmetas.jsx
+++ Front/src/components/Card-Suasmetas/cardsuasmetas.jsx
@@ -4,7 +4,8 @@ import styles from './cardsuasmetas.module.css'
 const CardSuasmetas = () => {
     return(
         <div className={styles.card_suasmetas}>
-        
+            <h1>Suas Metas de Autocuidado🎯</h1>
+            <h2>Defina pequenas metas para cuidar de você.</h2>
         </div>
     )
 }
