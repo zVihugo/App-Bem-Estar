318823015d11734a2e87a5b335766bca0a5c9edf
Victor Moreira <zvihugo@hotmail.com>
2025-05-03 15:43:24 -0300

    refactor: Alterando funcionalidades da pagina de login e adicionando logo

--- Front/src/components/Card-FormLogin/cardformlogin.jsx
+++ Front/src/components/Card-FormLogin/cardformlogin.jsx
@@ -6,29 +6,32 @@ const CardForm = () => {
         <div className={styles.card_formlogin}>
             <form className={styles.form}>
                 <h2 className={styles.title}> Login</h2>
-
-                <label htmlFor='email' className={styles.label}>email</label>
+                <label htmlFor='email' className={styles.label}>Email</label>
                 <input 
-                    type='email'
-                    id='emial'
+                    type='Email'
+                    id='Email'
                     name='email'
                     className={styles.input}
+                    onInvalid={(e) => e.target.setCustomValidity('Por favor, insira um endereço de email válido.')}
+                    onInput={(e) => e.target.setCustomValidity('')}
                     required
                 />  
 
-                <label htmlFor='senha' className={styles.label} >senha</label>
+                <label htmlFor='senha' className={styles.label} >Senha</label>
                 <input
                     type='password'
                     id='senha'
                     name='senha'
                     className={styles.input}
+                    onInvalid={(e) => e.target.setCustomValidity('Por favor, insira uma senha válida.')}
+                    onInput={(e) => e.target.setCustomValidity('')}
                     required
                 />
 
-                <button type='submit' className={styles.button}>entrar</button>
+                <button type='submit' className={styles.button}>Entrar</button>
             
                 <p className={styles.registro}>
-                    não pussi conta? <a href='/cadastro'>Crirar</a>
+                    Não possui conta? <a href='/Register'>Criar</a>
                 </p>
             </form>
         </div>
--- Front/src/pages/Login/Login.jsx
+++ Front/src/pages/Login/Login.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import './Login.css'
+import styles from './Login.module.css'
 import CardFormulario from '../../components/Card-FormLogin/cardformlogin'
 import CardImage from '../../components/Card-ImgLogin/cardimglogin'
 
+
 const Login = () => {
   return (
-    <div className='login'>
+    <div className={styles.login} >
+       <div className={styles.logo}>
+                      <img src="/Sun.svg" alt="Logo" className={styles.tam} />
+                          <h1 className={styles.title}>Bem Estar</h1>
+                     </div>
       <CardFormulario />
       <CardImage />
     </div>
