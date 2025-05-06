87705b3d6531c24c6a0b0a30b4b97f55d05e5fcd
Matheus Kodi Y. <matheuskodi2@gmail.com>
2025-05-06 15:43:28 -0300

    refactor(home): card autoavaliacao -> excluindo componente e adicionando diretamente na principal page

--- Front/src/pages/Principal/Principal.jsx
+++ Front/src/pages/Principal/Principal.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import CardAutoAvaliacao from '../../components/Card-AutoAvaliacao/cardautoavaliacao'
 import CardHumor from '../../components/Card-Humor/cardhumor'
 import CardAutoajuda from '../../components/Card-Autoajuda/cardautoajuda'
 import CardMetas from '../../components/Card-Metas/cardmetas'
 import styles from './principal.module.css'
+import { useNavigate } from 'react-router-dom';
 
 const Principal = () => {
+  const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/Autoavaliacao');
+    }
   return (
     <div className='principal'>
       <div className={styles.card_welcome}>
-                  <h1>Olá, Fulano! 😊 </h1>
-                  <h2>Como você está se sentido hoje?</h2>
+        <h1>Olá, Fulano! 😊 </h1>
+        <h2>Como você está se sentido hoje?</h2>
       </div>
-      <CardAutoAvaliacao />
+      <div className={styles.card_autoavaliacao}>
+        <p>😌 Faça sua autoavaliação de hoje</p>
+        <button onClick={handleClick}>Avaliar Agora</button>
+        <p>Progresso: <span>1/3 Avaliações esta semana</span></p>
+      </div>
       <CardHumor />
       <CardAutoajuda />
       <CardMetas />
