ca422821189f2b68936f49a9897ad49b361144fe
Matheus Kodi Y. <matheuskodi2@gmail.com>
2025-05-06 15:38:07 -0300

    refactor(principal): incorporar componentes diretamente na página principal -> componente welcome

--- Front/src/pages/Principal/Principal.jsx
+++ Front/src/pages/Principal/Principal.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import CardWelcome from '../../components/Card-Welcome/cardwelcome'
 import CardAutoAvaliacao from '../../components/Card-AutoAvaliacao/cardautoavaliacao'
 import CardHumor from '../../components/Card-Humor/cardhumor'
 import CardAutoajuda from '../../components/Card-Autoajuda/cardautoajuda'
 import CardMetas from '../../components/Card-Metas/cardmetas'
+import styles from './principal.module.css'
 
 const Principal = () => {
   return (
     <div className='principal'>
-      <CardWelcome />
+      <div className={styles.card_welcome}>
+                  <h1>Olá, Fulano! 😊 </h1>
+                  <h2>Como você está se sentido hoje?</h2>
+      </div>
       <CardAutoAvaliacao />
       <CardHumor />
       <CardAutoajuda />
