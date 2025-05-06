3b508ced855d3e8d214782d35b08ca2145ae291c
Matheus Kodi Y. <matheuskodi2@gmail.com>
2025-05-06 15:58:58 -0300

    refactor(principal): tornando o card autoavaliacao um componente dnv (percebi que é melhor assim mesmo)

--- Front/src/pages/Principal/Principal.jsx
+++ Front/src/pages/Principal/Principal.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import CardAutoajuda from '../../components/Card-Autoajuda/cardautoajuda'
 import CardMetas from '../../components/Card-Metas/cardmetas'
+import CardAutoavaliacao from '../../components/Card-AutoAvaliacao/cardautoavaliacao'
 import styles from './principal.module.css'
 import { useNavigate } from 'react-router-dom';
 
 const Principal = () => {
   const navigate = useNavigate();
-    const handleClick_autoavaliacao = () => {
-        navigate('/Autoavaliacao');
-    }
+
     const handleClick_relatorio = () => {
         navigate('/Relatorios');
     }
@@ -18,11 +17,7 @@ const Principal = () => {
         <h1>Olá, Fulano! 😊 </h1>
         <h2>Como você está se sentido hoje?</h2>
       </div>
-      <div className={styles.card_autoavaliacao}>
-        <p>😌 Faça sua autoavaliação de hoje</p>
-        <button onClick={handleClick_autoavaliacao}>Avaliar Agora</button>
-        <p>Progresso: <span>1/3 Avaliações esta semana</span></p>
-      </div>
+      <CardAutoavaliacao />
       <div className={styles.card_humor}>
             <p>📊 Seu humor nos últimos dias</p>
             <button onClick={handleClick_relatorio}>Ver Relatórios</button>
