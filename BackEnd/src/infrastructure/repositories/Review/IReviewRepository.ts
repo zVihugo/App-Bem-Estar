33e9a1747155f653b04daff0156a9f4ca37921e6
MauricioJrB <mjunior1205@gmail.com>
2025-05-07 21:00:02 -0300

    delete: removido metodo findAll da interface IReviewRepository

--- BackEnd/src/infrastructure/repositories/Review/IReviewRepository.ts
+++ BackEnd/src/infrastructure/repositories/Review/IReviewRepository.ts
@@ -3,7 +3,6 @@ import { Review } from '../../models/Review';
 
 export interface IReviewRepository {
   save(data: Review): Promise<Review>;
-  findAll(): Promise<Review[]>;
   findById(id: string): Promise<Review | null>;
   findAllByUserId(userId: string): Promise<Review[]>;
   update(id: string, data: Partial<ReviewProps>): Promise<Review>;
