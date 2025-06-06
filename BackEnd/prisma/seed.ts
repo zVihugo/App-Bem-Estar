35b62c2aa0d8a2b8ecb48e60cd0d94ca663bdd10
MauricioJrB <mjunior1205@gmail.com>
2025-06-05 22:36:50 -0300

    feat: adicionado Seed para criar um adminstrador no banco de dados

---
+++ BackEnd/prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+import { config } from '../src/config/config';
+import { passwordHash } from '../src/helpers/passwordHash';
+import { prisma } from '../src/infrastructure/bancoContext/prisma';
+
+class Seed {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async run() {
+    const hashedPassword = passwordHash(config.ADMIN_PASSWORD);
+
+    const adminExists = await this.prisma.user.findFirst({
+      where: { email: 'admin@admin.com' },
+    });
+
+    if (!adminExists) {
+      await this.prisma.user.create({
+        data: {
+          name: 'Administrador',
+          email: 'admin@admin.com',
+          password: hashedPassword,
+          dateOfBirth: '01/01/2025',
+          faculty: 'UTFPR',
+          course: 'ADMINSTRADOR',
+          role: 'ADMIN',
+        },
+      });
+      console.log('Admin criado com sucesso');
+    } else {
+      console.log('Admin já existe. Nenhuma ação necessária.');
+    }
+  }
+}
+
+const seed = new Seed(prisma);
+
+seed
+  .run()
+  .catch((err) => {
+    console.error('Erro ao rodar o seed:', err);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
