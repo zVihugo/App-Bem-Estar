285670523ef615a7a99d53e24665fb7e22e77c9f
MauricioJrB <mjunior1205@gmail.com>
2025-06-13 18:34:12 -0300

    refactor: Dicas atualizado

--- BackEnd/src/schemas/dicaSchemas.ts
+++ BackEnd/src/schemas/dicaSchemas.ts
@@ -1,16 +1,17 @@
-import { Type } from '@prisma/client';
+import { Tipo } from '@prisma/client';
 import { z } from 'zod';
 
 export const dicaCreateSchema = {
   body: z.object({
     userId: z.string().regex(/^[0-9a-fA-F]{24}$/, 'ID do usuário inválido'),
-    title: z.string().min(1, 'Título é obrigatório'),
-    imageUrl: z.string().min(1, 'URL da imagem é obrigatória'),
-    type: z.nativeEnum(Type, {
+    titulo: z.string().min(1, 'Título é obrigatório'),
+    thumbnailUrl: z.string().min(1, 'URL da imagem é obrigatória'),
+    tipo: z.nativeEnum(Tipo, {
       required_error: 'Campo obrigatório',
       invalid_type_error:
         'Insira uma das opções válidas: ler, assistir ou ouvir',
     }),
+    link: z.string().min(1, 'Link é obrigatório'),
   }),
 };
 
@@ -19,12 +20,13 @@ export const dicaUpdateSchema = {
     id: z.string().regex(/^[0-9a-fA-F]{24}$/, 'ID da dica inválido'),
   }),
   body: z.object({
-    title: z.string().min(1, 'Título é obrigatório'),
-    imageUrl: z.string().min(1, 'URL da imagem é obrigatória'),
-    type: z.nativeEnum(Type, {
+    titulo: z.string().min(1, 'Título é obrigatório'),
+    thumbnailUrl: z.string().min(1, 'URL da imagem é obrigatória'),
+    tipo: z.nativeEnum(Tipo, {
       required_error: 'Campo obrigatório',
       invalid_type_error:
         'Insira uma das opções válidas: ler, assistir ou ouvir',
     }),
+    link: z.string().min(1, 'Link é obrigatório'),
   }),
 };
