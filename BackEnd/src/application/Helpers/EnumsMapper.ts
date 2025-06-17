42c47ed23d7d79bfc51e289b8f14458cbe40671c
MauricioJrB <mjunior1205@gmail.com>
2025-06-17 18:03:01 -0300

    fix: corrigido erros de sintaxe

--- BackEnd/src/application/Helpers/EnumsMapper.ts
+++ BackEnd/src/application/Helpers/EnumsMapper.ts
@@ -8,15 +8,15 @@ import {
 export const dificuldadeMap: Record<DificuldadeParaDormir, number> = {
   nunca: 1,
   raramente: 2,
-  algumas_Vezes: 3,
+  algumas_vezes: 3,
   frequentemente: 4,
-  quase_Sempre: 5,
+  quase_sempre: 5,
 };
 
 export const acordaDescansadoMap: Record<AcordaDescansado, number> = {
   nunca: 1,
   raramente: 2,
-  algumas_Vezes: 3,
+  algumas_vezes: 3,
   sempre: 4,
 };
 
@@ -30,23 +30,23 @@ export const sofreSonoDiaMap: Record<SofreComSonoDuranteODia, number> = {
 export const usaTelaAntesMap: Record<UsaTelaAntesDeDormir, number> = {
   nunca: 1,
   raramente: 2,
-  algumas_Vezes: 3,
-  quase_Sempre: 4,
+  algumas_vezes: 3,
+  quase_sempre: 4,
   sempre: 5,
 };
 
 export const dificuldadeMapInvertido: Record<number, DificuldadeParaDormir> = {
   1: 'nunca',
   2: 'raramente',
-  3: 'algumas_Vezes',
+  3: 'algumas_vezes',
   4: 'frequentemente',
-  5: 'quase_Sempre',
+  5: 'quase_sempre',
 };
 
 export const acordaDescansadoInvertido: Record<number, AcordaDescansado> = {
   1: 'nunca',
   2: 'raramente',
-  3: 'algumas_Vezes',
+  3: 'algumas_vezes',
   4: 'sempre',
 };
 
@@ -60,7 +60,7 @@ export const sofreSonoDiaInvertido: Record<number, SofreComSonoDuranteODia> = {
 export const usaTelaAntesInvertido: Record<number, UsaTelaAntesDeDormir> = {
   1: 'nunca',
   2: 'raramente',
-  3: 'algumas_Vezes',
-  4: 'quase_Sempre',
+  3: 'algumas_vezes',
+  4: 'quase_sempre',
   5: 'sempre',
 };
